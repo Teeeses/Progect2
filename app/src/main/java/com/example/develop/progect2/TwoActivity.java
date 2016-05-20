b241e878573655650daf41d26f53c08a76113c60
teeeses <teeeses@mail.ru>
2016-05-20 20:49:20 +0300

    nearly make QR-code

--- app/src/main/java/com/example/develop/progect2/TwoActivity.java
+++ app/src/main/java/com/example/develop/progect2/TwoActivity.java
@@ -1,29 +1,66 @@
 package com.example.develop.progect2;
 
 import android.graphics.Camera;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import net.sourceforge.zbar.ImageScanner;
+import com.dlazaro66.qrcodereaderview.QRCodeReaderView;
 
 
-public class TwoActivity extends Fragment {
+
+public class TwoActivity extends Fragment implements QRCodeReaderView.OnQRCodeReadListener{
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
-    private Camera mCamera;
-    private ImageScanner scanner;
+
+    private View view;
+    private TextView myTextView;
+    private QRCodeReaderView mydecoderview;
+    private ImageView line_image;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_2, container, false);
+        view = inflater.inflate(R.layout.tab_fragment_1, container, false);
+
+
+        mydecoderview = (QRCodeReaderView) view.findViewById(R.id.qrdecoderview);
+        mydecoderview.setOnQRCodeReadListener(this);
+
+        myTextView = (TextView) view.findViewById(R.id.exampleTextView);
+
+        line_image = (ImageView) view.findViewById(R.id.red_line_image);
+
+        return view;
+    }
+
+    // Called when a QR is decoded
+    // "text" : the text encoded in QR
+    // "points" : points where QR control points are placed
+    @Override
+    public void onQRCodeRead(String text, PointF[] points) {
+        myTextView.setText(text);
+    }
+
+
+    // Called when your device have no camera
+    @Override
+    public void cameraNotFound() {
 
     }
 
+    // Called when there's no QR codes in the camera preview image
+    @Override
+    public void QRCodeNotFoundOnCamImage() {
+
+    }
 
 }
